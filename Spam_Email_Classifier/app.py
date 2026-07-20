eabe944894f8178d9b191d8215d9a818a2ee19af
quackhackk <tgyurmed04@gmail.com>
2026-07-20 21:56:26 +0530

    Add files via upload

--- Spam_Email_Classifier/app.py
+++ Spam_Email_Classifier/app.py
@@ -1,11 +1,20 @@
 import streamlit as st
 import joblib
 from pathlib import Path
+import traceback
 
 BASE_DIR = Path(__file__).resolve().parent
 
 st.write("App directory:", BASE_DIR)
 st.write("Files:", [p.name for p in BASE_DIR.iterdir()])
 
-model = joblib.load(BASE_DIR / "random_forest_model.pkl")
-vectorizer = joblib.load(BASE_DIR / "tfidf_vectorizer.pkl")
+try:
+    model = joblib.load(BASE_DIR / "random_forest_model.pkl")
+    st.success("Model loaded successfully!")
+
+    vectorizer = joblib.load(BASE_DIR / "tfidf_vectorizer.pkl")
+    st.success("Vectorizer loaded successfully!")
+
+except Exception as e:
+    st.error(type(e).__name__)
+    st.code(traceback.format_exc())
